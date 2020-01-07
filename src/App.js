81cef82358c6b6e278efbb71bd1e2e899b31aa76
sssgordon <gso7@uwo.ca>
2020-01-07 19:13:07 +0100

    Go home push

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./App.css";
 
 import About from "./components/about";
+import Header from "./components/header";
 
 function App() {
-    return <About />;
+    return (
+        <div>
+            <Header />
+            <About />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(props) {
+    return (
+        <div className="header">
+            <div className="header__title">gordon so</div>
+        </div>
+    );
+}
