52be7315e3081bd425a89de645ae675f4a642ba9
sssgordon <gso7@uwo.ca>
2020-01-09 15:11:30 +0100

    Style project page

--- src/components/Outcry.js
+++ src/components/Outcry.js
@@ -16,7 +16,7 @@ export default function Outcry(props) {
                 <h1 className="heading-primary secondary-stroke">
                     Project Outcry
                 </h1>
-                <h3 className="heading-tertiary secondary-fill">
+                <h3 className="heading-secondary secondary-fill">
                     Full-stack web app<br></br>Google Assistant
                 </h3>
             </div>
@@ -33,32 +33,41 @@ export default function Outcry(props) {
                 </video>
             </div>
             <div className="project__description">
-                <p className="tagline">
+                <p className="tagline secondary-fill">
                     Ensuring protestors’ right to legal counsel
                 </p>
-                <hr className="divider" />
+                <div className="divider secondary-background u-margin-bottom-small"></div>
                 <p className="paragraph">
                     Project Outcry is an app made for Hong Kong protestors to
-                    inform their lawyers the moment they get arrested. They only
-                    need to sign up on the website with a unique alias and tell
-                    that to Google Assistant at the moment of arrest. Outcry
-                    will immediately send an email to the user's lawyer.
+                    inform their lawyers the moment they get arrested. Before
+                    taking to the streets, they would sign up on the website and
+                    create a personal profile including a unique alias. At the
+                    moment of arrest simply shout the alias to Google Assistant,
+                    Project Outcry will immediately send an email to notify the
+                    user's lawyer of the incident.
                 </p>
                 <p className="paragraph">
-                    This app is made with JavaScript, React, Redux and Google
-                    Dialogflow. You're welcome to learn more about its technical
-                    features by visiting the{" "}
-                    <a
-                        href="https://github.com/sssgordon/project-outcry-client"
-                        className="link"
-                    >
+                    Built for a 4-day Hackathon at Codaisseur, this app won the
+                    second-most-favorited app by popular vote. I used
+                    JavaScript, React, Redux and Google Dialogflow for this
+                    project. You can learn more about the technical features by
+                    visiting the{" "}
+                    <a href="#" className="link">
                         repository
                     </a>{" "}
                     on GitHub.
                 </p>
             </div>
-            <img src={outcry_1} alt="landing page" />
-            <img src={outcry_2} alt="user details page" />
+            <img
+                className="project__image u-margin-bottom-medium"
+                src={outcry_1}
+                alt="landing page"
+            />
+            <img
+                className="project__image"
+                src={outcry_2}
+                alt="user details page"
+            />
             <div className="footnote">&#9400; 2020 Gordon So</div>
         </div>
     );
