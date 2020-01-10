ce173e053590d39d4540590d57cabf2ee702a625
sssgordon <gso7@uwo.ca>
2020-01-10 12:29:02 +0100

    Birch project page (incomplete)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import About from "./components/About";
@@ -9,16 +9,20 @@ import Outcry from "./components/Outcry";
 import ScrollToTop from "./components/ScrollToTop";
 import Natours from "./components/Natours";
 import Jotto from "./components/Jotto";
+import Birch from "./components/Birch";
 
 function App(props) {
     return (
         <div className="app">
             <ScrollToTop />
-            <Route exact path="/jotto" component={Jotto} />
-            <Route exact path="/natours" component={Natours} />
-            <Route exact path="/project-outcry" component={Outcry} />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
+            <Birch />
+            {/* <Switch>
+                <Route exact path="/jotto" component={Jotto} />
+                <Route exact path="/natours" component={Natours} />
+                <Route exact path="/project-outcry" component={Outcry} />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+            </Switch> */}
         </div>
     );
 }
---
+++ src/components/Birch.js
@@ -0,0 +1,101 @@
+import React from "react";
+import Header from "./Header";
+
+import video_mp4 from "../assets/birch_video.mp4";
+import video_webm from "../assets/birch_video.webm";
+
+export default function Birch(props) {
+    // const pathname = props.location.pathname;
+    return (
+        <div className="project u-margin-top-large">
+            <Header
+            // pathname={pathname}
+            />
+            <div className="project__heading">
+                <h1 className="project__heading--title heading-primary primary-stroke">
+                    Birch
+                </h1>
+                <h3 className="project__heading--description heading-secondary primary-fill">
+                    Full-stack web shop<br></br>React-Redux
+                </h3>
+            </div>
+            <div className="project__video">
+                <video
+                    className="project__video--content"
+                    loop
+                    autoPlay
+                    muted
+                    preload="metadata"
+                >
+                    <source src={video_mp4} type="video/mp4" />
+                    <source src={video_webm} type="video/webm" />
+                    Your browser is not supported!
+                </video>
+            </div>
+            <div className="project__description">
+                <p
+                    className="tagline primary-fill"
+                    data-aos="fade"
+                    data-aos-delay="0"
+                    data-aos-duration="1500"
+                    data-aos-easing="ease"
+                >
+                    Plants for the adventurous
+                </p>
+                <div
+                    className="divider primary-background u-margin-bottom-small"
+                    data-aos="extendRight"
+                    data-aos-delay="800"
+                    data-aos-duration="1400"
+                    data-aos-easing="ease-out-quart"
+                ></div>
+                <p
+                    className="paragraph"
+                    data-aos="fade"
+                    data-aos-delay="1300"
+                    data-aos-duration="1300"
+                    data-aos-easing="ease"
+                    data-aos-anchor=".divider"
+                >
+                    Birch is a web shop for audacious plant-lovers. We sell
+                    plants that thrive on extreme weather conditions. From your
+                    living room to your garden, our diverse selection nurtures
+                    your daring soul with a taste of adventure.{" "}
+                    <a href="#" target="_blank" className="link">
+                        Bon Voyage
+                    </a>
+                    !
+                </p>
+                <p
+                    className="paragraph"
+                    data-aos="fade"
+                    data-aos-delay="1300"
+                    data-aos-duration="1300"
+                    data-aos-easing="ease"
+                    data-aos-anchor=".divider"
+                >
+                    This project began as a three-day group assignment to
+                    practice on JavaScript, React and Redux.{" "}
+                    <a
+                        href="https://github.com/MelissaDTH"
+                        target="_blank"
+                        className="link"
+                    >
+                        Melissa
+                    </a>{" "}
+                    and I then decided to expand the project into a full-stack
+                    application and created a backend with ExpressJS, Sequelize
+                    and PostgreSQL. You can learn more by heading to the GitHub{" "}
+                    <a
+                        href="https://github.com/sssgordon/birch"
+                        target="_blank"
+                        className="link"
+                    >
+                        repository
+                    </a>
+                    .
+                </p>
+            </div>
+        </div>
+    );
+}
