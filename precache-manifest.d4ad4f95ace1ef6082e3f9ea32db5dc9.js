b8ac6ff65e407651aab2fb938f610e564bbb41b2
sssgordon <gso7@uwo.ca>
2020-01-12 15:11:31 +0100

    Updates

---
+++ precache-manifest.d4ad4f95ace1ef6082e3f9ea32db5dc9.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9de1b83bfd5170f20da021eb55011e",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "3e3c0a1f7e5c4fc3f045",
+    "url": "/portfolio-website/static/css/main.0c1f61cd.chunk.css"
+  },
+  {
+    "revision": "82fdf064c9369a4f391c",
+    "url": "/portfolio-website/static/js/2.5932da6b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio-website/static/js/2.5932da6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e3c0a1f7e5c4fc3f045",
+    "url": "/portfolio-website/static/js/main.6a44a052.chunk.js"
+  },
+  {
+    "revision": "3bbe5c50059ce7530136",
+    "url": "/portfolio-website/static/js/runtime-main.20e331af.js"
+  },
+  {
+    "revision": "c70bb9ce9309f194aeaa4ca251f839e1",
+    "url": "/portfolio-website/static/media/birch_1.c70bb9ce.png"
+  },
+  {
+    "revision": "c014d8a43efbd4cb1df6573fea2e05dc",
+    "url": "/portfolio-website/static/media/birch_2.c014d8a4.png"
+  },
+  {
+    "revision": "4ebef43133b078dd075a1752447ac9cc",
+    "url": "/portfolio-website/static/media/birch_img.4ebef431.jpg"
+  },
+  {
+    "revision": "07e1eba93150045ed13a1cc4994f6cc5",
+    "url": "/portfolio-website/static/media/birch_video.07e1eba9.mp4"
+  },
+  {
+    "revision": "3c37f4563ba3ec3ce6c2fedb92516164",
+    "url": "/portfolio-website/static/media/birch_video.3c37f456.webm"
+  },
+  {
+    "revision": "ec4b451f8725ced2065a9d0c3a518c98",
+    "url": "/portfolio-website/static/media/jotto_1.ec4b451f.png"
+  },
+  {
+    "revision": "ea8042ad30647b0af75a6ca36ba949b2",
+    "url": "/portfolio-website/static/media/jotto_img.ea8042ad.jpg"
+  },
+  {
+    "revision": "752b699324ecfa97fb475399ba890631",
+    "url": "/portfolio-website/static/media/lisa_1.752b6993.png"
+  },
+  {
+    "revision": "03cb49fa7616e77c1faf5c59aec25e73",
+    "url": "/portfolio-website/static/media/lisa_img.03cb49fa.jpg"
+  },
+  {
+    "revision": "66cf2369c51e55dd411d199a3fcec132",
+    "url": "/portfolio-website/static/media/lisa_video.66cf2369.mp4"
+  },
+  {
+    "revision": "e632fe14c75417142db99e096fc6e2b2",
+    "url": "/portfolio-website/static/media/lisa_video.e632fe14.webm"
+  },
+  {
+    "revision": "84a3346cf559ee399e2c9203b7f2785a",
+    "url": "/portfolio-website/static/media/natours_1.84a3346c.png"
+  },
+  {
+    "revision": "40a87397f058b7ba9f13c12787ebcf18",
+    "url": "/portfolio-website/static/media/natours_2.40a87397.png"
+  },
+  {
+    "revision": "d6241a32ac7c340a1769073579a3ac85",
+    "url": "/portfolio-website/static/media/natours_3.d6241a32.png"
+  },
+  {
+    "revision": "5b9219f7ebe984383199933a35f79cdd",
+    "url": "/portfolio-website/static/media/natours_img.5b9219f7.jpg"
+  },
+  {
+    "revision": "a5df00fdce962c5d9a234de108556154",
+    "url": "/portfolio-website/static/media/natours_video.a5df00fd.webm"
+  },
+  {
+    "revision": "b5c21fca06c670b4d9e814bb14b5b9d2",
+    "url": "/portfolio-website/static/media/natours_video.b5c21fca.mp4"
+  },
+  {
+    "revision": "b3083223ca6ec8acc8400adbc0bb7dc6",
+    "url": "/portfolio-website/static/media/outcry_1.b3083223.png"
+  },
+  {
+    "revision": "5afede92cfe5eeca3a22b6a5173c7abc",
+    "url": "/portfolio-website/static/media/outcry_2.5afede92.png"
+  },
+  {
+    "revision": "bd786bef52ca7e2a39912d698681356a",
+    "url": "/portfolio-website/static/media/outcry_img.bd786bef.jpg"
+  },
+  {
+    "revision": "65a097e25dcf6aa72e49f6131dc34b64",
+    "url": "/portfolio-website/static/media/outcry_video.65a097e2.mp4"
+  },
+  {
+    "revision": "f6a475c3e59ed2b4bdf4a8b09c4d3afa",
+    "url": "/portfolio-website/static/media/outcry_video.f6a475c3.webm"
+  }
+]);
